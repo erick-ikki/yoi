123fd5f8d59095384b75e12630f42d3da6dd066d
Juan Reynoso <juanreynoso@ikkiware.com>
2013-05-03 10:16:29 -0500

    Fixed bug on add-topic-on-db function.

--- src/topic-func-lib.lisp
+++ src/topic-func-lib.lisp
@@ -3,8 +3,7 @@
 (defun add-topic-on-db (tname tdescription imgpath)
   (db-connection) ; Se realiza la conexion a la base de datos
   (query (:insert-into 'topics
-	  :set
-	  'topic_id (:nextval "topic_id") ; Se ejecuta una secuencia con el nombre de topic_id
+	  :set	 
 	  'creation_time (my-date (get-universal-time)) 
 	  'topic_name tname
 	  'topic_description tdescription
